3af34a23acd13b49421bb42e5c8cf759cccc5427
Tatiana Zihindula <tatianazihindula@gmail.com>
2018-04-06 11:26:48 +0100

    quick temporary script to edit my todo task byt need more powerfull language

---
+++ todo.sh
@@ -0,0 +1,9 @@
+RED="\033[1;31m"
+BLK="\033[1;30m"
+RST="\033[1;0m"
+U="\033[4m"
+case "$1" in
+    *"APP"*) ORANGE="\033[1;33m"; BLU="\033[1;34m";;
+    *"TASK"*) ORANGE="\033[1;32m";BLU="\033[1;35m";;
+esac
+printf "$RED\rTODO$RST : $ORANGE$1$RST$BLK\tNAME$RST: $BLU$U$2$RST\t$BLK DESC$RST: $3\n" >> ~/.todo
